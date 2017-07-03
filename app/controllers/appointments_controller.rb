a8b65057224a6cac87dfb673d697f9e9c7f2058e
Jack Bittiner <jackbittiner@hotmail.com>
2017-07-03 15:28:57 +0100

    can create new appointments and then displays them

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -4,5 +4,17 @@ class AppointmentsController < ApplicationController
     @appointments = Appointment.order('appt_time ASC')
     @appointment = Appointment.new
   end
-  
+
+  def create
+    @appointment = Appointment.create(appointment_params)
+    @appointments = Appointment.order('appt_time ASC')
+    redirect_to :root
+  end
+
+  private
+
+  def appointment_params
+    params.require(:appointment).permit(:title, :appt_time)
+  end
+
 end
