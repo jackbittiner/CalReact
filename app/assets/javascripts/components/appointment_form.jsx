e44c0a7fc5b3518196814a6756044f0cf9c5d0d0
Jack Bittiner <jackbittiner@hotmail.com>
2017-07-05 16:50:56 +0100

    Can change state of appointment form

--- app/assets/javascripts/components/appointment_form.jsx
+++ app/assets/javascripts/components/appointment_form.jsx
@@ -1,16 +1,26 @@
 var AppointmentForm = React.createClass({
+  
+  handleChange: function(e) {
+    var name = e.target.name;
+    obj = {};
+    obj[name] = e.target.value;
+    this.props.onUserInput(obj);
+  },
+
   render: function() {
-    return(
+    return (
       <div>
         <h2>Make a new appointment</h2>
         <form>
           <input name='title' placeholder='Appointment Title'
-            value={this.props.input_title} />
-          <input name='appt_time' placeholder='Date & Time'
-            value={this.props.input_appt_time} />
+            value={this.props.input_title}
+            onChange={this.handleChange} />
+          <input name='appt_time' placeholder='Date and Time'
+            value={this.props.input_appt_time}
+            onChange={this.handleChange}  />
           <input type='submit' value='Make Appointment' />
         </form>
       </div>
     )
   }
-})
+});
